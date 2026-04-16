df1f321776e4aa3ed73e619fa6284cc5ffd2817e
kwangs7243 <kwangs7243@gmail.com>
2026-04-16 17:22:15 +0900

    feat : 전처리 실패 기능과 분석용 데이터생성기능의 허용값 및 기준 통일

--- DataAnalyzer.py
+++ DataAnalyzer.py
@@ -49,13 +49,15 @@ class DataAnalyzer():
 
         df["type_str"] = df["type_raw"].str.strip().str.replace(" ", "", regex=False).str.lower()
         df["type_map"] = df["type_str"].replace({
-            'income':'수입','refund':'수입','expense':'지출'})
+            'income':'수입','refund':'수입','bonus':'수입','expense':'지출'})
         
+
         df["category_str"] = df["category_raw"].str.strip().str.replace(" ", "", regex=False).str.lower()
         df["category_map"] = df["category_str"].replace({
             'food':'식비','cafe':'카페','shopping':'쇼핑',
             'bonus':'급여','salary':'급여',
-            'transport':'교통'})
+            'transport':'교통', 'gift':'선물',
+            'travel': '여행'})
 
         is_MAN = df["amount_raw"].str.contains("만")
         is_CHUN = df["amount_raw"].str.contains("천")
@@ -68,10 +70,15 @@ class DataAnalyzer():
     # 전처리 실패항목 체크하기
     def find_invalid_rows(self):
         df = self.df
+        valid_type = ["수입", "지출"]
+        valid_category = ['쇼핑', '카페', '급여', '교통', '식비', '선물', '여행']
         date_invalid = df["date_dt"].isna()
-        type_invalid = ~df["type_map"].isin(["수입","지출"]) | df["type_map"].isna()
+        type_invalid = ~df["type_map"].isin(valid_type) | df["type_map"].isna()
         amount_invalid = df["amount_num"].isna()
-        category_invalid = (df["category_map"].str.strip() == "") | df["category_map"].isna()  
+        category_invalid =( (~df["category_map"].isin(valid_category)) | 
+                           (df["category_map"].str.strip() == "") | 
+                           (df["category_map"].isna())
+                           )
         content_invalid = df["content"].isna()
         invalid_mask = date_invalid | type_invalid | amount_invalid | category_invalid | content_invalid
 
@@ -83,12 +90,17 @@ class DataAnalyzer():
 
         analysis_data = self.df[["date_dt","year","month","type_map",
                             "category_map","amount_num","content"]].copy()
+        
         analysis_data = analysis_data.rename(
                             columns=
                                 {"date_dt":"date", "type_map":"type", 
                                 "category_map":"category", "amount_num":"amount"})
         
         analysis_data = analysis_data.dropna(axis=0)
+        valid_type = ["수입", "지출"]
+        valid_category = ['쇼핑', '카페', '급여', '교통', '식비', '선물', '여행']
+        analysis_data = analysis_data[analysis_data["type"].isin(valid_type)]
+        analysis_data = analysis_data[analysis_data["category"].isin(valid_category)]
 
         return analysis_data
     
--- test2.py
+++ test2.py
@@ -4,16 +4,24 @@ from DataAnalyzer import DataAnalyzer
 da = DataAnalyzer()
 da.load_data("messy_expense_data_large.csv")
 da.preprocess_data()
-invalid =  da.find_invalid_rows()
-invalid_view = invalid[["date_raw","type_raw","amount_raw","category_raw","content"]]
-da.save_data(da.df,"debugging_data.csv")
+invalid = da.find_invalid_rows()
+
+invalid_filtered = invalid[
+    ~(invalid["date_raw"].isna() |
+      invalid["type_raw"].isna() |
+      invalid["amount_raw"].isna() |
+      invalid["category_raw"].isna() |
+      invalid["content"].isna())
+]
+# da.save_data(invalid_filtered,"invalid_data.csv")
+da.save_data(da.get_view_data(),"view_data.csv")
 # print(da.df.loc[da.df["amount_raw"].str.contains("천"),"amount_num"])
 # print(da.df["amount_str"])
 # print(invalid_view)
 # print(da.df["date_dt"])
 # print(da.get_view_data())
 # print(da.df.iloc[14])
-
+# print(da.get_view_data()["category"].unique())
 # print(f"전체 행 수 : {len(da.df)}")
 # print(f"정상 행 수: {len(da.get_view_data())}")
 # print(f"실패 행 수: {len(invalid_view)}")
