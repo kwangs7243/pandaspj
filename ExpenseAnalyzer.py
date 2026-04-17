f1e1de476edf6c2fe8b38040e5b911c9fd79f6ea
kwangs7243 <kwangs7243@gmail.com>
2026-04-17 14:16:39 +0900

    feat : 기간조회, 카테고리조회, 타입조회 기능추가 refactor : 분석조회모델안에 메서드를 기능별로 위치조정 fix : 사용자의 정렬상태요청에 대비 출력데이터생성기능이 정렬을 담당하도록 수정

--- ExpenseAnalyzer.py
+++ ExpenseAnalyzer.py
@@ -1,28 +1,48 @@
 import pandas as pd
-from DataAnalyzer import DataAnalyzer
-da = DataAnalyzer()
 class ExpenseAnalyzer:
+    def __init__(self,data:pd.DataFrame):
+        self.df = data
 
-    def get_view_data(self,data):
+    def get_view_data(self,data,sort_by="date",order="desc"):
         view_data = data[["date","type","category","amount","content"]]
+        view_data:pd.DataFrame = view_data.sort_values(by=sort_by,ascending=(order == "asc"))
         return view_data
+#======================================조회 기능===========================================
 
-        # 년,월 필터 데이터
+    # 년,월 조회
     def filter_by_year_month(self,year,month):
-        analysis_data = da.get_analysis_data()
+        analysis_data = self.df
         filtered_data : pd.DataFrame  = analysis_data[((analysis_data["year"]==year) &
                                                         (analysis_data["month"]==month))]
-        filtered_data = filtered_data.sort_values(by="date")
-        filtered_data = self.get_view_data(filtered_data)
 
         return filtered_data
     
+    # 기간 조회
+    def filter_by_date_range(self,start_date, end_date):
+        analysis_data = self.df
+        filtered_data = (analysis_data[(analysis_data["date"] >= start_date  ) &
+                                       (analysis_data["date"] <= end_date)])
+        return filtered_data
+    # 카테고리 조회
+    def filter_by_category(self,category_name):
+        analysis_data = self.df
+        filtered_data = analysis_data[analysis_data["category"]==category_name]
+
+        return filtered_data
     
+    # 타입 조회
+    def filter_by_type(self,type_name):
+        analysis_data = self.df
+        filtered_data = analysis_data[analysis_data["type"]==type_name]
+
+        return filtered_data
+#======================================조회 기능===========================================
+
     # 타입별 top n위까지 데이터생성
     def get_top_n_by_type(self, type_name, n):
-        analysis_data : pd.DataFrame = da.get_analysis_data()
+        analysis_data = self.df
         filtered_data : pd.DataFrame = analysis_data[analysis_data["type"]==type_name]
-        top_data = (
+        top_data  = (
             filtered_data
             .sort_values(by="amount",ascending=False)
             .head(n)
@@ -30,11 +50,11 @@ class ExpenseAnalyzer:
         top_data = self.get_view_data(top_data)
 
         return top_data
-    
+
     # 키워드 검색 
     def filter_by_keyword(self, keyword=""):
         keyword = keyword.strip()
-        analysis_data = da.get_analysis_data()
+        analysis_data = self.df
         if not keyword:
             return self.get_view_data(analysis_data)
         
@@ -46,24 +66,24 @@ class ExpenseAnalyzer:
 
     # 년,월 요약데이터 (수입 지출 총액 요약) 저장시 인덱스 True
     def summary_by_year_month(self,year,month):
-        analysis_data = da.get_analysis_data()
+        analysis_data = self.df
         filtered_data : pd.DataFrame  = analysis_data[((analysis_data["year"]==year) &
                                                         (analysis_data["month"]==month))]
         summary_data = filtered_data.groupby("type")[["amount"]].sum()
 
         return summary_data
-    
+
     # 카테고리,타입 요약 (카테고리 ,타입별 총액) 저장시 인덱스 True
     def summary_by_category_type(self):
-        analysis_data = da.get_analysis_data()
+        analysis_data = self.df
         summary_data = analysis_data.groupby(["category", "type"])[["amount"]].sum()
         summary_data = summary_data.sort_values(by="type")
 
         return summary_data
-    
+
     # 카테고리 요약 (카테고리별 총액) 저장시 인덱스 True
     def summary_by_category(self, type_name):
-        analysis_data = da.get_analysis_data()
+        analysis_data = self.df
         filtered_data : pd.DataFrame = analysis_data[analysis_data["type"] == type_name]
         summary_data = filtered_data.groupby("category")[["amount"]].sum()
         summary_data = summary_data.sort_values(by="amount", ascending=False)
