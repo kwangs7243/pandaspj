73f1bda837a26eec64674e939c451e5904ca9380
kwang <kwangs7243@gmil.com>
2026-04-19 01:07:18 +0900

    feat: 월별요약기능, 특정월별요약기능,카테고리별요약기능,카테고리별건수요약기능 추가

--- src/ExpenseAnalyzer.py
+++ src/ExpenseAnalyzer.py
@@ -23,13 +23,13 @@ class ExpenseAnalyzer:
         start_date = pd.to_datetime(start_date,"%Y-%m-%d")
         end_date = pd.to_datetime(end_date,"%Y-%m-%d")
         filtered_data = (
-            analysis_data[(analysis_data["date"] >= start_date  )&
+            analysis_data[(analysis_data["date"] >= start_date  ) &
                             (analysis_data["date"] <= end_date)]
                         )
         
         return filtered_data
     
-    # 수입,지출별 조회
+    # 타입 조회
     def filter_by_type(self,type_name):
         analysis_data = self.df
         filtered_data = analysis_data[analysis_data["type"]==type_name]
@@ -50,7 +50,7 @@ class ExpenseAnalyzer:
 
         return filtered_data
     
-    # 키워드 검색 
+    # 키워드 조회 
     def filter_by_keyword(self, keyword=""):
         keyword = keyword.strip()
         analysis_data = self.df
@@ -63,6 +63,25 @@ class ExpenseAnalyzer:
 #======================================조회 기능===========================================
 
 #======================================요약 기능===========================================
+    # 전체요약
+    def summary_total(self):
+        analysis_data = self.df
+
+        summary_data = analysis_data.groupby("type")["amount"].sum()
+
+        total_imcome = summary_data["수입"]
+        total_expense = summary_data["지출"]
+        net_income = total_imcome - total_expense
+
+        summary_total_data = pd.Series(
+            {
+            "총수입" : total_imcome,
+            "총지출" : total_expense,
+            "순수익" : net_income}
+            )
+
+        return summary_total_data
+    
     # 월별 수입,지출 순이익 요약
     def summary_by_month(self):
         analysis_data = self.df
@@ -75,56 +94,41 @@ class ExpenseAnalyzer:
         summary_data = (
             analysis_data.groupby(["year_month","type"])["amount"]
             .sum()
-            .unstack()
-            .fillna(0)
+            .unstack(fill_value=0)
+            .reindex(columns=["수입","지출"],fill_value=0)
             )
         summary_data["순이익"] = summary_data["수입"] - summary_data["지출"]
         
 
         return summary_data
+                # type              수입       지출       순이익
+                # year_month
+                # 2025-01     25710000  1504400  24205600
+                # 2025-02     21616000  1305500  20310500
 
-    # 특정 년,월 별 요약데이터 (수입 지출 총액 요약) 저장시 인덱스 True
+    # 특정 년,월 별 요약
     def summary_by_year_month(self,year,month):
+        summary_data = self.summary_by_month()
+        target = pd.Period(f"{year}-{month:02d}",freq="M")
+
+        return summary_data.loc[[target]]
+                # type              수입       지출       순이익
+                # year_month
+                # 2025-01     25710000  1504400  24205600
+
+    # 카테고리별 요약
+    def summary_by_category(self,type_name=None):
         analysis_data = self.df
-        filtered_data : pd.DataFrame  = analysis_data[((analysis_data["year"]==year) &
-                                                        (analysis_data["month"]==month))]
-        summary_data = filtered_data.groupby("type")[["amount"]].sum()
-
-        return summary_data
-    # 전체 수입,지출 순이익 요약
-    def summary_total(self):
-        analysis_data = self.df
-        total_income = analysis_data.groupby("수입")[["amount"]].sum()
-        total_expense = analysis_data.groupby("지출")[["amount"]].sum()
-        return total_income, total_expense
-    # 타입별 top n위까지 데이터생성
-    def get_top_n_by_type(self, type_name, n):
-        analysis_data = self.df
-        filtered_data : pd.DataFrame = analysis_data[analysis_data["type"]==type_name]
-        top_data  = (
-            filtered_data
-            .sort_values(by="amount",ascending=False)
-            .head(n)
-            )
-        top_data = self.get_view_data(top_data)
-
-        return top_data
-
-
-
-    # 카테고리,타입 요약 (카테고리 ,타입별 총액) 저장시 인덱스 True
-    def summary_by_category_type(self):
-        analysis_data = self.df
-        summary_data = analysis_data.groupby(["category", "type"])[["amount"]].sum()
-        summary_data = summary_data.sort_values(by="type")
+        summary_data = analysis_data.groupby("category")["amount"].sum()
+        if type_name is not None:
+            return summary_data.loc[[type_name]]
 
         return summary_data
 
-    # 카테고리 요약 (카테고리별 총액) 저장시 인덱스 True
-    def summary_by_category(self, type_name):
+    # 카테고리별 건수 요약
+    def summary_count_by_category(self):
         analysis_data = self.df
-        filtered_data : pd.DataFrame = analysis_data[analysis_data["type"] == type_name]
-        summary_data = filtered_data.groupby("category")[["amount"]].sum()
-        summary_data = summary_data.sort_values(by="amount", ascending=False)
+        summary_data = analysis_data.groupby("category")["amount"].count()
 
         return summary_data
+#======================================요약 기능===========================================
