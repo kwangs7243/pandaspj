6adb18a888da7803aed6f3c994f94380d665a0f8
kwang <kwangs7243@gmil.com>
2026-04-19 23:23:15 +0900

    fix: 전체요약 기능 구현방식을 재사용성을위해 pivot_table로 작성

--- src/ExpenseAnalyzer.py
+++ src/ExpenseAnalyzer.py
@@ -11,51 +11,45 @@ class ExpenseAnalyzer:
 
     # 년,월별 조회
     def filter_by_year_month(self,year:int,month:int):
-        analysis_data = self.df
-        filtered_data = analysis_data[((analysis_data["year"]==year) &
-                                        (analysis_data["month"]==month))]
+        filtered_data = self.df[((self.df["year"]==year) &
+                                        (self.df["month"]==month))]
 
         return filtered_data
     
     # 기간별 조회
     def filter_by_date_range(self,start_date:pd.Timestamp, end_date:pd.Timestamp):
-        analysis_data = self.df
         filtered_data = (
-            analysis_data[(analysis_data["date"] >= start_date  ) &
-                            (analysis_data["date"] <= end_date)]
+            self.df[(self.df["date"] >= start_date  ) &
+                            (self.df["date"] <= end_date)]
                         )
         
         return filtered_data
     
     # 타입 조회
     def filter_by_type(self,type_name:str):
-        analysis_data = self.df
-        filtered_data = analysis_data[analysis_data["type"]==type_name]
+        filtered_data = self.df[self.df["type"]==type_name]
 
         return filtered_data
     
     # 카테고리별 조회
     def filter_by_category(self,category_name:str):
-        analysis_data = self.df
-        filtered_data = analysis_data[analysis_data["category"]==category_name]
+        filtered_data = self.df[self.df["category"]==category_name]
 
         return filtered_data
     
     # 최소금액이상 조회
     def filter_by_min_amount(self,min_amount:int):
-        analysis_data = self.df
-        filtered_data = analysis_data[analysis_data["amount"] >= min_amount]
+        filtered_data = self.df[self.df["amount"] >= min_amount]
 
         return filtered_data
     
     # 키워드 조회 
     def filter_by_keyword(self, keyword:str=""):
         keyword = keyword.strip()
-        analysis_data = self.df
         if not keyword:
-            return analysis_data
+            return self.df
         
-        filtered_data = analysis_data[analysis_data["content"].str.contains(keyword,na=False)]
+        filtered_data = self.df[self.df["content"].str.contains(keyword,na=False)]
 
         return filtered_data
 #======================================조회 기능===========================================
@@ -63,34 +57,29 @@ class ExpenseAnalyzer:
 #======================================요약 기능===========================================
     # 전체요약
     def summary_total(self):
-        analysis_data = self.df
+        summary_data = self.df.pivot_table(columns="type", values="amount", aggfunc="sum", fill_value=0)
+        summary_data = summary_data.rename(
+                            columns={
+                                "수입" : "총수입",
+                                "지출" : "총지출"
+                            }
+                        )
+        summary_data["순이익"] = summary_data["총수입"] - summary_data["총지출"]
 
-        summary_data = analysis_data.groupby("type")["amount"].sum()
-
-        total_income = summary_data.get("수입", 0)
-        total_expense = summary_data.get("지출", 0)
-        net_income = total_income - total_expense
-
-        summary_total_data = pd.Series(
-            {
-            "총수입" : total_income,
-            "총지출" : total_expense,
-            "순이익" : net_income}
-            )
-
-        return summary_total_data
+        return summary_data
+                # type          총수입       총지출        순이익
+                # amount  336224000  25285000  310939000
     
     # 월별 수입,지출 순이익 요약
     def summary_by_month(self):
-        analysis_data = self.df
-        # summary_data = analysis_data.pivot_table(
+        # summary_data = self.df.pivot_table(
         #     index="month",columns="type",values="amount",aggfunc="sum")
         # 월별 수입/지출 합계를 피벗 테이블 형태로 요약
         # pivot_table은 집계와 형태 변환을 한 번에 처리할 수 있어서
         # 단순 요약표를 만들 때 편하다.
         # 다만 복잡한 집계나 후처리가 많아지면 groupby + agg + unstack 방식이 더 유연하다.
         summary_data = (
-            analysis_data.groupby(["year_month","type"])["amount"]
+            self.df.groupby(["year_month","type"])["amount"]
             .sum()
             .unstack(fill_value=0)
             .reindex(columns=["수입","지출"],fill_value=0)
@@ -107,7 +96,7 @@ class ExpenseAnalyzer:
     # 특정 년,월 별 요약
     def summary_by_year_month(self,year:int,month:int):
         summary_data = self.summary_by_month()
-        target = pd.Period(f"{year}-{month:02d}",freq="M")
+        target = pd.Period(year=year, month=month ,freq="M")
 
         return summary_data.loc[[target]]
                 # type              수입       지출       순이익
@@ -116,9 +105,8 @@ class ExpenseAnalyzer:
 
     # 카테고리별 요약
     def summary_by_category_type(self,type_name:str=None):
-        analysis_data = self.df
         summary_data = (
-            analysis_data.groupby(["category","type"])["amount"]
+            self.df.groupby(["category","type"])["amount"]
             .sum()
             .unstack(fill_value=0)
             .reindex(columns=["수입","지출"],fill_value=0)
@@ -130,8 +118,15 @@ class ExpenseAnalyzer:
 
     # 카테고리별 건수 요약
     def summary_count_by_category(self):
-        analysis_data = self.df
-        summary_data = analysis_data.groupby("category").size()
+        summary_data = self.df.groupby("category").size()
 
         return summary_data
 #======================================요약 기능===========================================
+
+
+#======================================순위 기능===========================================
+    # 타입별 상위 N개
+    def get_top_n_by_type(self,type_name:str, n:int):
+        top_date = self.df.groupby
+
+
